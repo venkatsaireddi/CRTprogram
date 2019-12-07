b58305524e9314c6c3fd6f37cefa6aa05194d819
venkatsaireddi <58408575+venkatsaireddi@users.noreply.github.com>
2019-12-07 12:01:18 +0530

    Add files via upload

---
+++ sumofeveendigits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n,r,sum=0;
+scanf("%d",&n);
+do
+{
+r=n%10;
+if(r%2==0)
+sum=sum+r;
+n=n/10;
+   
+}while(n!=0);
+printf("%d",sum);
+return 0;
+}
+
