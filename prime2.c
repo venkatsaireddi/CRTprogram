291d80cdfb102e84f746e75efa8c4440a3f35d96
venkatsaireddi <58408575+venkatsaireddi@users.noreply.github.com>
2019-12-07 10:22:23 +0530

    Add files via upload

---
+++ prime2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,i,flag=0;
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+
+if(flag==0)
+printf("prime");
+else
+printf("not a prime");
+return 0;
+}
+
