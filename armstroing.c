696b0ebeacaeabb1996b110971e24063b7e4a32f
venkatsaireddi <58408575+venkatsaireddi@users.noreply.github.com>
2019-12-07 11:09:40 +0530

    Add files via upload

---
+++ armstroing.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,r,sum=0,num;
+scanf("%d",&n);
+num=n;
+while(n!=0)
+{
+r=n%10;
+sum=sum+r*r*r;
+n=n/10;
+}
+if(num==sum)
+printf("amstrong");
+else
+printf(" not amstrong");
+
+return 0;
+}
+
