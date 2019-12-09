0efba8a55bb50e8b1b2c00d654911b9a5dab0a2e
venkatsaireddi <58408575+venkatsaireddi@users.noreply.github.com>
2019-12-09 10:26:29 +0530

    Add files via upload

---
+++ leftrightshift.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+int a=10,b=10;
+printf("a<<2=%d",a<<2);//left shift
+//shifting the bits on to the left side and assinging o's to the right side
+printf("b>>2=%d ",b>>2);
+return 0;
+}
+
